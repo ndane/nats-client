83b991c8d2a8702a9035e1ac2da4c727f576fb75
Nathan Dane <nathan@nathandane.co.uk>
2019-10-15 21:40:26 +0100

    It works

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"io/ioutil"
 
@@ -8,22 +10,28 @@ import (
 )
 
 func main() {
-	server := *flag.String("c", nats.DefaultURL, "Specify a NATS connection string")
+	server := flag.String("c", nats.DefaultURL, "Specify a NATS connection string")
 
-	file := *flag.String("f", "", "Specify a JSON file to publish to a subject")
-	subject := *flag.String("s", "", "Specify a subject to publish to")
+	file := flag.String("f", "", "Specify a JSON file to publish to a subject")
+	subject := flag.String("s", "", "Specify a subject to publish to")
+	flag.Parse()
 
-	conn, _ := nats.Connect(server)
+	conn, _ := nats.Connect(*server)
 
-	if len(file) > 0 && len(subject) > 0 {
-		data, err := ioutil.ReadFile(file)
+	if len(*file) > 0 && len(*subject) > 0 {
+		data, err := ioutil.ReadFile(*file)
 		if err != nil {
 			panic(err)
 		}
-		err = conn.Publish(subject, data)
+		if !json.Valid(data) {
+			panic(errors.New("Invalid JSON supplied"))
+		}
+		err = conn.Publish(*subject, data)
 		if err != nil {
 			panic(err)
 		}
-		return
 	}
+
+	conn.Drain()
+	conn.Close()
 }
